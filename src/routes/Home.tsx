f9d4e452df349ee0204df24b98fc329da05c5d2c
Your Name01~ <brenoleitepereira@gmail.com>
2023-05-21 07:59:21 -0300

    Feat: framer-motion adicionado

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,15 +1,20 @@
 import HeaderHome from "../components/Headers/Home";
 import MainHome from "../components/Mains/Home";
 import { Main } from "../globalStyle";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
       <HeaderHome />
       <Main>
         <MainHome />
       </Main>
-    </div>
+    </motion.div>
   );
 };
 
