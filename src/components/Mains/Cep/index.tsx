d58f772b74ae675eb6d625401f59d80f72fef6fd
Your Name01~ <brenoleitepereira@gmail.com>
2023-05-22 20:26:08 -0300

    Feat: Página cep feita, faltando responsividade

--- src/components/Mains/Cep/index.tsx
+++ src/components/Mains/Cep/index.tsx
@@ -1,37 +1,71 @@
 import { useForm, SubmitHandler } from "react-hook-form";
-import { StyledDivCep } from "./style";
+import { LottieDiv, ResponseDiv, StyledDivCep } from "./style";
 import send from "../../../assets/send.png";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Lottie from "lottie-react";
+import search from "../../../assets/search.json";
+import lixo from "../../../assets/lixo.png";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 type Inputs = {
   cep: string;
-  replace: any;
+};
+
+type CepData = {
+  city: string;
+  neighborhood: string;
+  state: string;
+  street: string;
 };
 
 const MainCep = () => {
   const [cep, setCep] = useState("");
+  const [responseCep, setResponseCep] = useState<CepData | null>(null);
+
+  const handleClick = () => {
+    setResponseCep(null);
+    setCep("");
+  };
 
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => {
     setCep(data.cep);
+    reset();
   };
 
   useEffect(() => {
-    const responseCep = axios
+    axios
       .get(`https://brasilapi.com.br/api/cep/v1/${cep}`)
       .then((response) => {
-        console.log(response.data);
+        setResponseCep(response.data);
+        toast.success("Cep encontrado");
       })
       .catch((error) => {
         console.error(error);
+        console.log(error.response.data.message);
+        if (
+          error.response.data.message ==
+          "CEP deve conter exatamente 8 caracteres."
+        ) {
+          toast.error("CEP deve conter exatamente 8 caracteres...");
+        }
+        if (
+          error.response.data.message ==
+          "Todos os serviços de CEP retornaram erro."
+        ) {
+          toast.error("CEP inexistente.");
+        }
       });
 
     console.log(cep);
+    console.log(responseCep);
   }, [cep]);
 
   return (
@@ -41,16 +75,36 @@ const MainCep = () => {
       <h3>Digite aqui o CEP desejado</h3>
 
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input placeholder="Cep desejado..." {...register("cep")} />
+        <input
+          type="number"
+          placeholder="Cep desejado..."
+          {...register("cep")}
+        />
         {errors.cep && <span>Esse campo é necessário...</span>}
         <button type="submit">
           <img src={send} alt="Enviar" />
         </button>
       </form>
 
-      {/* <div>
-        <h4>Dados</h4>
-      </div> */}
+      <ToastContainer autoClose={2000} />
+      {responseCep == null ? (
+        <LottieDiv>
+          <Lottie animationData={search} />
+        </LottieDiv>
+      ) : (
+        <ResponseDiv>
+          <h4>Dados</h4>
+
+          <div>
+            <span>Cidade: {responseCep?.city}</span>
+            <span>Bairro: {responseCep?.neighborhood}</span>
+            <span>Estado: {responseCep?.state}</span>
+            <span>Rua: {responseCep?.street}</span>
+          </div>
+
+          <img src={lixo} alt="Lixeira" onClick={handleClick} />
+        </ResponseDiv>
+      )}
     </StyledDivCep>
   );
 };
--- src/components/Mains/Cep/style.ts
+++ src/components/Mains/Cep/style.ts
@@ -46,7 +46,7 @@ export const StyledDivCep = styled.div`
         transition: transform 0.2s ease-in-out;
 
         &::placeholder {
-            font-size: 24px;
+            font-size: 16px;
         }
 
         &:hover {
@@ -55,6 +55,58 @@ export const StyledDivCep = styled.div`
 
     }
     }
+`
 
-    
+export const ResponseDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 0px 0px 4px 0px;
+    border-radius: 10px;
+    gap: 20px;
+    position: relative;
+
+    /* width: 100%; */
+    min-width: 499px;
+    min-height: 299px;
+    padding: 20px;
+    font-size: var(--text2);
+    transition: transform 0.2s ease-in-out;
+
+    div {
+        display: flex;
+        width: 80%;
+        
+        /* min-height: 200px; */
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        margin-top: 20px;
+
+        span {
+            width: 50%;
+        }
+    }
+
+    img {
+            width: 34px;
+            height: 34px;
+            position: absolute;
+            right: 20px;
+            top: 20px;
+            transition: transform 0.2s ease-in-out;
+         cursor: pointer;
+
+            &:hover {
+            transform: scale(1.1);
+        }
+
+        }
+`
+
+export const LottieDiv = styled.div`
+    width: 499px;
+    height: 299px;
+    transition: transform 0.2s ease-in-out;
 `
