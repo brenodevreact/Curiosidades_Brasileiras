7e639161c9367b0484a6d3886a11f422ace127a5
Your Name01~ <brenoleitepereira@gmail.com>
2023-05-23 06:45:32 -0300

    Feat: Motion adicionado no cnpj e ddd

--- src/routes/Cnpj.tsx
+++ src/routes/Cnpj.tsx
@@ -1,12 +1,17 @@
 import HeaderCnpj from "../components/Headers/Cnpj";
 import MainCnpj from "../components/Mains/Cnpj";
+import { motion } from "framer-motion";
 
 const Cnpj = () => {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
       <HeaderCnpj />
       <MainCnpj />
-    </>
+    </motion.div>
   );
 };
 
--- src/routes/Ddd.tsx
+++ src/routes/Ddd.tsx
@@ -1,12 +1,17 @@
 import HeaderDdd from "../components/Headers/Ddd";
 import MainDDD from "../components/Mains/Ddd";
+import { motion } from "framer-motion";
 
 const Ddd = () => {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
       <HeaderDdd />
       <MainDDD />
-    </>
+    </motion.div>
   );
 };
 
